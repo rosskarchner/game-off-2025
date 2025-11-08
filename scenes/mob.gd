d78f2f4743032d9ff9082e75fe15511353d68697
Ross Karchner <ross@karchner.com>
2025-11-08 13:12:07 -0500

    catchup

--- scenes/mob.gd
+++ scenes/mob.gd
@@ -3,33 +3,44 @@ enum FacingDirections {Right,Left}
 
 @onready var sprite_2d: Sprite2D = $Sprite2D
 
-const SPEED = 550.0
-const JUMP_VELOCITY = -450.0
+const SPEED = 400.0
+const JUMP_VELOCITY = -550.0
 
 var facing: FacingDirections = FacingDirections.Right
 var last_direction:float=0.0
 
+var should_flap:=false
+@onready var baseline_height:= global_position.y
+
+func check_flap():
+	if global_position.y > baseline_height +75:
+		should_flap = true
+	
 func _physics_process(delta: float) -> void:
 	# Add the gravity.
 	if not is_on_floor():
 		velocity += get_gravity() * delta
 
 	# Handle jump.
-	if Input.is_action_just_pressed("ui_accept"):
+	if should_flap:
 		velocity.y = JUMP_VELOCITY
+		should_flap = false
 
 	# Get the input direction and handle the movement/deceleration.
 	# As good practice, you should replace UI actions with custom gameplay actions.
-	var direction := Input.get_axis("ui_left", "ui_right")
-	if direction:
-		last_direction  = direction
-		velocity.x = direction * SPEED
-		sprite_2d.flip_h = velocity.x < 0
-		if direction > 0:
-			facing = FacingDirections.Right
-		else:
-			facing = FacingDirections.Left
-	elif is_on_floor():
+	#var direction := Input.get_axis("ui_left", "ui_right")
+	var direction:Vector2
+	if facing == FacingDirections.Right:
+		direction = Vector2.RIGHT
+	else:
+		direction = Vector2.LEFT
+		
+
+	velocity.x = (direction * SPEED).x
+	sprite_2d.flip_h = velocity.x < 0
+
+
+	if is_on_floor():
 		velocity.x = move_toward(velocity.x, 0, SPEED*delta*6)
 		
 
